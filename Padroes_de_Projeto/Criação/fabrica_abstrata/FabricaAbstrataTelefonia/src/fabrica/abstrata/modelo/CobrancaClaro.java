6cc91915193b4de515f24dcb0058a3bdbac0e9af
JhovanyMurgia <98408956+JhovanyMurgia@users.noreply.github.com>
2022-09-12 17:05:29 -0300

    Create CobrancaClaro.java

---
+++ Padroes_de_Projeto/Criação/fabrica_abstrata/FabricaAbstrataTelefonia/src/fabrica/abstrata/modelo/CobrancaClaro.java
@@ -0,0 +1,13 @@
+fabricação de pacotes. abstrata. modelo;
+
+importação fabrica. abstrata. modelo. Cobranca;
+
+
+classe pública CobrancaClaro implementa Cobranca {
+
+    @Substituir
+    vazio público  cobrar (int telefone) {
+        O sistema. para fora. println ("Cobrar via claro");
+    }
+    
+}
